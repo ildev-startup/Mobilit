8f31fdc0e2c9b0d56f4d7b1be339ad60ed4b46d8
Gui Gessele <guigesselefl@gmail.com>
2018-01-22 01:08:05 -0200

    Some modifications in the UI to improve UX. Started the implementation of the Sign In button. Created a Class (Validator) that validates either the email and the password. Maybe it will be required to modify the email regular expression.

--- app/src/main/java/com/ildev/mobilit/UserActivity.java
+++ app/src/main/java/com/ildev/mobilit/UserActivity.java
@@ -1,17 +1,24 @@
 package com.ildev.mobilit;
 
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+
+import static com.ildev.mobilit.utils.Validator.validateEmail;
+import static com.ildev.mobilit.utils.Validator.validatePassword;
 
 public class UserActivity extends AppCompatActivity {
 
     // Declaration of Variables
-    TextView mAppTextView, mTextViewSignUp;
-    EditText mEditTextPassword, mEditTextEmail;
     Button mButtonSignIn;
+    TextView mTextViewSignUp;
+    TextInputLayout mEmailWrapper, mPasswordWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +26,56 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
         // Getting the references to the UI Elements
-        mEditTextEmail = (EditText) findViewById(R.id.EditTextEmail);
-        mEditTextPassword = (EditText) findViewById(R.id.EditTextPassword);
         mButtonSignIn = (Button) findViewById(R.id.ButtonSignIn);
         mTextViewSignUp = (TextView) findViewById(R.id.TextViewSignUp);
+        mEmailWrapper = (TextInputLayout) findViewById(R.id.emailWrapper);
+        mPasswordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
 
+        // Setting hints
+        mEmailWrapper.setHint(getString(R.string.email));
+        mPasswordWrapper.setHint(getString(R.string.password));
+
+        // Setting SignIn Button ActionListener
+        mButtonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(); // Hiding the Keyboard
+
+                // Getting Email and Password
+                String email = mEmailWrapper.getEditText().getText().toString();
+                String password = mPasswordWrapper.getEditText().getText().toString();
+
+                // Setting Errors to null
+                mEmailWrapper.setError(null);
+                mPasswordWrapper.setError(null);
+
+                // Validating the email and password
+                if(!validateEmail(email) && !validatePassword(password) ){
+                    // Invalid email and password
+                    mEmailWrapper.setError("Invalid email");
+                    mPasswordWrapper.setError("Invalid password");
+                }else if(!validateEmail(email)){
+                    // Invalid email
+                    mEmailWrapper.setError("Invalid email");
+                }else if(!validatePassword(password)){
+                    // Invalid password
+                    mPasswordWrapper.setError("Invalid password");
+                }else{
+                    // Valid email and password
+                    // TODO: Login method
+                }
+            }
+        });
     }
 
 
+    // Method that hides the Keyboard
+    private void hideKeyboard(){
+        // Check if no view has focus
+        View view = getCurrentFocus(); // Get the view with focus or return null
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
 }
---
+++ app/src/main/java/com/ildev/mobilit/utils/Validator.java
@@ -0,0 +1,29 @@
+package com.ildev.mobilit.utils;
+
+/**
+ * Created by guilherme on 21/01/18.
+ */
+
+import java.util.regex.Pattern;
+
+/**
+ * This class will store the methods that will be used to validate
+ * the user in the Login Page
+ */
+public class Validator {
+
+    private static final String EMAIL_PATTERN = "[a-zA-Z0-9]+[._a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]*[a-zA-Z]*@[a-zA-Z0-9]{2,8}.[a-zA-Z.]{2,6}";
+    private static final int MIN_CHARACTERS = 6;
+
+    // Method that checks if the String Email matches with the EMAIL_PATTERN
+    public static boolean validateEmail(String email){
+        return Pattern.matches(EMAIL_PATTERN, email);
+    }
+
+    // Method that checks if the Password has more than 5 digits
+    public static boolean validatePassword(String password){
+        return password.length() >= MIN_CHARACTERS;
+    }
+
+
+}
